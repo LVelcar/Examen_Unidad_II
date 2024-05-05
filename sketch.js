754842b47e2d7d5fda2f0bcd246740888b3c5fac
LVelcar <luisvelcar9@gmail.com>
2024-05-05 15:57:36 -0700

    Creación de proyecto base

---
+++ sketch.js
@@ -0,0 +1,14 @@
+// Luis Alberto Velázquez Carballo.
+// Alexander Higuera Sanabria.
+// Universidad Autónoma De Baja California Sur.
+// Ing. Desarrollo De Software.
+// T.M.
+// Examen Unidad II.
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
