72212eff2bc68f8706ff4f3ff0afa9284f703a71
retbleed <hsa.lapaz@gmail.com>
2024-05-06 17:38:24 -0700

    Algoritmos DDA y Bresenham

--- sketch.js
+++ sketch.js
@@ -119,4 +119,47 @@ function drawCircle(xc, yc, r) {
   }
 }
 
+// Algoritmo de Bresenham para dibujar una línea
+function drawLine(x0, y0, x1, y1) {
+  let dx = abs(x1 - x0);
+  let dy = abs(y1 - y0);
+  let sx = (x0 < x1) ? 1 : -1;
+  let sy = (y0 < y1) ? 1 : -1;
+  let err = dx - dy;
+
+  while (true) {
+    point(x0, y0);
+
+    if (x0 == x1 && y0 == y1)
+      break;
+
+    let e2 = 2 * err;
+    if (e2 > -dy) {
+      err -= dy;
+      x0 += sx;
+    }
+
+    if (e2 < dx) {
+      err += dx;
+      y0 += sy;
+    }
+  }
+}
+
+// Algoritmo DDA para dibujar una línea
+function drawLineDDA(x0, y0, x1, y1) {
+  let dx = x1 - x0;
+  let dy = y1 - y0;
+  let steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+  let xIncrement = dx / steps;
+  let yIncrement = dy / steps;
+  let x = x0;
+  let y = y0;
+
+  for (let i = 0; i <= steps; i++) {
+    point(x, y);
+    x += xIncrement;
+    y += yIncrement;
+  }
+}
 
